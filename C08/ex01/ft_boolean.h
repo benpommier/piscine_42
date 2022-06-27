99c87b2ee7e91e8ff97a41306145450341c5479a
Benjamin Pommier <98982509+benpommier@users.noreply.github.com>
2022-06-27 18:05:11 +0200

    Add files via upload

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpommier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 10:14:48 by bpommier          #+#    #+#             */
+/*   Updated: 2022/06/23 10:14:50 by bpommier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define EVEN(nbr) (nbr) % 2 == 0
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+
+typedef int	t_bool;
+
+#endif
---
+++ C08/ex05/ft_show_tab.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpommier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 11:21:48 by bpommier          #+#    #+#             */
+/*   Updated: 2022/06/27 15:23:55 by bpommier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_stock_str.h"
+#include <unistd.h>
+
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av);
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	counter;
+
+	counter = 0;
+	while (str[counter])
+	{
+		write(1, &str[counter], 1);
+		counter++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str != 0 && par)
+	{
+		ft_putstr(par[i].str);
+		write(1, "\n", 1);
+		ft_putnbr(par[i].size);
+		write(1, "\n", 1);
+		ft_putstr(par[i].copy);
+		write(1, "\n", 1);
+		i++;
+	}
+}
